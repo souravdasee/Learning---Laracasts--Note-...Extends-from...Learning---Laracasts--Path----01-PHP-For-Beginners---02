1cfe82b2e89fea846da0990f1c69050d9a0f3a3a
Sourav Das <developer.souravdas.ee@gmail.com>
2023-04-20 16:48:01 +0530

    Render the Notes and Note Page

--- views/note.view.php
+++ views/note.view.php
@@ -1,14 +1,14 @@
 <?php require('partials/head.php') ?>
 <?php require('partials/nav.php') ?>
-
+<?php require('partials/banner.php') ?>
 
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <h1 class="text-2xl font-bold">Sorry. Page not found</h1>
-
-        <p class="mt-4">
-            <a href="/" class="text-blue-500 hover:underline">Go back home</a>
+        <p class="mb-6">
+            <a href="/notes" class="text-blue-500 hover:underline">go back...</a>
         </p>
+
+        <p><?= $note['body'] ?></p>
     </div>
 </main>
 
