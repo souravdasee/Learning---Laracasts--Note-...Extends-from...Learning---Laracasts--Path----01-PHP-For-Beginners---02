30fe368c33eef0fde44917d5b1b3652cf2086dc5
Sourav Das <developer.souravdas.ee@gmail.com>
2023-04-20 16:52:09 +0530

    Always Escape Untrusted Input

--- views/note.view.php
+++ views/note.view.php
@@ -8,7 +8,7 @@
             <a href="/notes" class="text-blue-500 hover:underline">go back...</a>
         </p>
 
-        <p><?= $note['body'] ?></p>
+        <p><?= htmlspecialchars($note['body']) ?></p>
     </div>
 </main>
 
