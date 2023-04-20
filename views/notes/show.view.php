10b35513abc4e3046bd9172e6978107dbc9b1d9c
Sourav Das <developer.souravdas.ee@gmail.com>
2023-04-20 17:02:22 +0530

    Updating With PATCH Requests

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -10,11 +10,9 @@
 
         <p><?= htmlspecialchars($note['body']) ?></p>
 
-        <form class="mt-6" method="POST">
-            <input type="hidden" name="_method" value="DELETE">
-            <input type="hidden" name="id" value="<?= $note['id'] ?>">
-            <button class="text-sm text-red-500">Delete</button>
-        </form>
+        <footer class="mt-6">    
+            <a href="/note/edit?id=<?= $note['id'] ?>" class="inline-flex justify-center rounded-md bg-gray-500 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Edit</a>
+        </footer>
     </div>
 </main>
 
